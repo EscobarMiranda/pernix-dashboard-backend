30af7cd21de09a598ccf821ace8b0d35dd4d788f
Kevin Escobar Miranda <info@EscobarMiranda.com>
2016-11-11 15:19:53 -0600

    Add index.html

--- src/main/resources/initializeDB.sql
+++ src/main/resources/initializeDB.sql
@@ -1,2 +1,7 @@
 INSERT INTO public.usertype(id, name) VALUES (1, 'User');
 INSERT INTO public.usertype(id, name) VALUES (2, 'Administrator');
+
+INSERT INTO public.ontrack(id, name) VALUES (1, 'On track');
+INSERT INTO public.ontrack(id, name) VALUES (2, 'Delayed');
+INSERT INTO public.ontrack(id, name) VALUES (3, 'Stopped');
+INSERT INTO public.ontrack(id, name) VALUES (4, 'Finished');
